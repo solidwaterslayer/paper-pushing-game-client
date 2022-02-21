1ae60e33cfa85fd8bea601cdf243b3066f00e2be
Michael Tran <solidwater969@gmail.com>
2022-02-21 17:56:53 -0500

    feature: add answer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,21 @@ import { LevelService } from './level.service';
 })
 export class AppComponent {
   public level: any
+  public answer: number[]
 
   public constructor(private levelService: LevelService) {
     this.levelService.getLevel().subscribe(
       (level: any) => { this.level = level },
       (httpErrorResponse: HttpErrorResponse) => { console.log(httpErrorResponse.message) }
     )
+    this.answer = [0, 4]
+  }
+
+  public addAnswer(answer: number) {
+    if (!this.answer.includes(answer)) {
+      this.answer.push(answer)
+    }
+
+    this.answer = this.answer.slice(this.answer.length - 2, this.answer.length)
   }
 }
